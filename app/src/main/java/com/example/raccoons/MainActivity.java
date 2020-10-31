2115980fba956afe55b110ab52ac50dea8329aa6
RoundRaccoon <ddaninn01@gmail.com>
2020-10-31 22:39:05 +0200

    Magazine GridView

--- app/src/main/java/com/example/raccoons/MainActivity.java
+++ app/src/main/java/com/example/raccoons/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.raccoons;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView cardView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, CategoriiActivity.class));
+        setContentView(R.layout.activity_magazine);
+
+        cardView1 = (CardView) findViewById(R.id.shops1);
+
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this,CategoriiActivity.class));
+
+            }
+        });
+
     }
+
 }

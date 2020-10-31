29dee721eb4e9b12628c13f88735155c7af7cebb
Andrei Tudor <andrei.tudor9229@gmail.com>
2020-10-31 15:59:13 +0000

    cardViews + layouts

---
+++ app/src/main/java/com/example/raccoons/CategoriiActivity.java
@@ -0,0 +1,14 @@
+package com.example.raccoons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategoriiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categorii);
+    }
+}

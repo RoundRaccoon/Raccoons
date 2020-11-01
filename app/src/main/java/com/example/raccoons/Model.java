fa381e5a4d1676d313a41a915fe1d6887ed8aa07
Andrei Tudor <andrei.tudor9229@gmail.com>
2020-11-01 05:50:26 +0000

    alex&slider

--- app/src/main/java/com/example/raccoons/Model.java
+++ app/src/main/java/com/example/raccoons/Model.java
@@ -3,6 +3,7 @@ package com.example.raccoons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -13,44 +14,32 @@ public class Model extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_model);
 
+        ImageView alex = (ImageView) findViewById(R.id.alex);
+        alex.getLayoutParams().width = 500;
+        alex.getLayoutParams().height = 500;
+
         SeekBar sk1 = (SeekBar) findViewById(R.id.seekBar1);
         sk1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                TextView t=(TextView)findViewById(R.id.textView1);
-                t.setText(String.valueOf(i));
+            public void onProgressChanged(SeekBar seekBar1, int progress, boolean fromUser) {
+                alex.getLayoutParams().width = progress * 10;
+                alex.getLayoutParams().height = progress * 10;
+                alex.requestLayout();
+                TextView t = (TextView)findViewById(R.id.textView1);
+                t.setText(String.valueOf(progress));
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+            public void onStartTrackingTouch(SeekBar seekBar1) {
 
             }
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+            public void onStopTrackingTouch(SeekBar seekBar1) {
 
             }
         });
 
-        SeekBar sk2 = (SeekBar) findViewById(R.id.seekBar2);
-        sk2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                TextView t=(TextView)findViewById(R.id.textView2);
-                t.setText(String.valueOf(i));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
     }
 }
